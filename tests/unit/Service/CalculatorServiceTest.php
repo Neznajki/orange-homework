ef9609b5034b4748969b20e58d6f1af8a0d373a0
neznajka <neznajki@gmail.com>
2020-07-24 15:42:20 +0300

    * added cover of saving add function

--- tests/unit/Service/CalculatorServiceTest.php
+++ tests/unit/Service/CalculatorServiceTest.php
@@ -2,9 +2,14 @@
 
 namespace App\Tests\Service\CalculatorService;
 
+use App\Entity\History;
+use App\Factory\HistoryEntityFactory;
 use App\Repository\HistoryRepository;
 use App\Service\CalculatorService;
 use App\Service\CalculatorValidatorService;
+use AspectMock\Test;
+use Exception;
+use LogicException;
 use PHPUnit\Framework\MockObject\MockObject;
 use Tests\Neznajka\Codeception\Engine\Abstraction\AbstractSimpleCodeceptionTest;
 
@@ -15,7 +20,7 @@ class CalculatorServiceTest extends AbstractSimpleCodeceptionTest
      * @dataProvider getData_testCalculate
      * @param string $formula
      * @param float $expectingResult
-     * @throws \Exception
+     * @throws Exception
      */
     public function testCalculate(string $formula, float $expectingResult)
     {
@@ -26,6 +31,21 @@ class CalculatorServiceTest extends AbstractSimpleCodeceptionTest
                 'addResult'
             ]
         );
+        $mock = $this->createMockExpectsNoUsage(History::class);
+
+        $function = function($f, $r) use ($mock, $formula, $expectingResult){
+            if (preg_replace('/\\s/','',$formula) != $f) {
+                throw new LogicException("formula does not match");
+            }
+
+            if (number_format($r, 2) != number_format($expectingResult, 2)) {
+                throw new LogicException("formula does not match");
+            }
+
+            return $mock;
+        };
+
+        Test::double(HistoryEntityFactory::class, ['createEntity' => $function]);
 
         $class = new CalculatorService(new CalculatorValidatorService(),$historyRepository);
 
