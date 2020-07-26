77c4457bc1b8bcbd3fd02f1c1b76e620b085b1e3
neznajka <neznajki@gmail.com>
2020-07-26 11:01:06 +0300

    * simplified repository

--- src/Repository/HistoryRepository.php
+++ src/Repository/HistoryRepository.php
@@ -5,11 +5,25 @@ namespace App\Repository;
 
 
 use App\Entity\History;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
 
-class HistoryRepository extends AbstractRepository
+/**
+ * @method History|null find($id, $lockMode = null, $lockVersion = null)
+ * @method History|null findOneBy(array $criteria, array $orderBy = null)
+ * @method History[]    findAll()
+ * @method History[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class HistoryRepository extends ServiceEntityRepository
 {
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, History::class);
+    }
+
     /**
      * @param History $entity
      * @throws ORMException
